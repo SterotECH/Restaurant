8567f0952183d6f91eda50522908a8ba10547f7d
SterotECH <agyeisterogh@gmail.com>
2022-01-26 10:54:10 +0000

    Navbar Responsive done

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let navbar = document.querySelector('.header .navbar');
+let menuBtn = document.querySelector('#menu-btn');
+
+menuBtn.onclick = () => {
+    menuBtn.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
